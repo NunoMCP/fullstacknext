3d1e6b2b56544644df40f94fc620e66cc92d026b
NunoMCP <nunomcp1998@gmail.com>
2022-11-24 12:42:21 +0000

    default

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex h-screen w-screen flex-col">
+      <div className=""></div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Home;
